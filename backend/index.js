0da3e25b731ba80e3bd07681257cf2250ee8c331
Bartlomiej Kloza <bartlomiej.kloza@bbc.co.uk>
2019-12-11 17:26:00 +0000

    wip

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,31 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes/routes.js');
 
 const app = express();
 const port = 3001;
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const routes = require("./routes/routes.js");
 
 mongoose
-  .connect("mongodb://localhost:27017/robson-flowerbox-db", {
+  .connect('mongodb://localhost:27017/test', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(
     () => {
-      console.log("Database is connected");
+      console.log('Database is connected');
     },
     err => {
       console.log(`Can not connect to the database${err}`);
     }
   );
-mongoose.set("useFindAndModify", false);
+mongoose.set('useFindAndModify', false);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/api", routes);
+app.use('/api', routes);
 
 app.listen(port, () =>
-  console.log(`Express REST API listening on port ${port}!`)
+  console.log(`Express server listening on port ${port}!`)
 );
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,18 +1,27 @@
 const express = require('express');
-
-const router = express.Router();
-const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
 const User = require('../schemas/userSchema');
 
-dotenv.config();
+const router = express.Router();
 
-router.get('/getimages', (req, res) => {
-  Image.find({}, (err, images) => {
-    if (err) {
-      return res.status(400).json(err);
-    }
-    return res.status(200).json(images);
-  });
+router.post('/newuser', async (req, res) => {
+  try {
+    const encryptedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      username: req.body.username,
+      password: encryptedPassword,
+    });
+    await newUser.save(newUser);
+
+    return res.status(200).json({ message: 'success' });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
+});
+
+router.get('/getuser', async (req, res) => {
+  res.status(200).json({ msg: 'hello' });
 });
 
 module.exports = router;
